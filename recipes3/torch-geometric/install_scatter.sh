477ee551ed7d3ed647aa2586c83a136b48075703
Matthias Barth <b4rthm@github.com>
2019-10-10 18:58:45 +0200

    torch-geometric recipes

---
+++ recipes3/torch-geometric/install_scatter.sh
@@ -0,0 +1,14 @@
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+echo "..................INSTALLING TORCH_SCATTER......................"
+
+#git clone https://github.com/rusty1s/pytorch_scatter.git
+#cd pytorch_scatter
+#$PYTHON setup.py install
+#cd ..
+
---
+++ recipes3/torch-geometric/run_test.sh
@@ -0,0 +1,14 @@
+# Die folgende Zeile wirft einen Fehler, wenn sie in build.sh steht
+pip install googledrivedownloader gdist cython rdflib==4.2.2 plyfile==0.7
+
+#git clone https://github.com/rusty1s/pytorch_cluster.git
+#git clone https://github.com/rusty1s/pytorch_sparse.git
+#git clone https://github.com/rusty1s/pytorch_scatter.git
+
+echo "...................................TEST............................................"
+
+
+#git clone https://github.com/rusty1s/pytorch_geometric.git
+#cd pytorch_geometric
+#$PYTHON setup.py test
+#cd ..
