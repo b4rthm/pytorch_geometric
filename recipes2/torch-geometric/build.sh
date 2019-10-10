769ffcf98d17f9107c5768f99e2167536b89bc12
Matthias Barth <b4rthm@github.com>
2019-10-10 18:58:45 +0200

    torch-geometric recipes

---
+++ recipes2/torch-geometric/build.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/rusty1s/pytorch_scatter.git
+cd pytorch_scatter
+$PYTHON setup.py install
+cd ..
+
+git clone https://github.com/rusty1s/pytorch_cluster.git
+cd pytorch_cluster
+$PYTHON setup.py install
+cd ..
+
+git clone https://github.com/rusty1s/pytorch_sparse.git
+cd pytorch_sparse
+$PYTHON setup.py install
+cd ..
+
