79d1c349fbd2f525cad94afab815bfef5536d209
Matthias Barth <b4rthm@github.com>
2019-10-14 12:27:03 +0200

    merge conflict

--- examples/node2vec.py
+++ examples/node2vec.py
@@ -9,6 +9,7 @@ from torch_geometric.nn import Node2Vec
 
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 dataset = 'Cora'
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
@@ -17,7 +18,7 @@ data = dataset[0]
 loader = DataLoader(torch.arange(data.num_nodes), batch_size=128, shuffle=True)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = Node2Vec(data.num_nodes, embedding_dim=2, walk_length=20,
+model = Node2Vec(data.num_nodes, embedding_dim=128, walk_length=20,
                  context_size=10, walks_per_node=10)
 model, data = model.to(device), data.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -44,7 +45,7 @@ def test():
     return acc
 
 
-for epoch in range(1, 1001):
+for epoch in range(1, 101):
     loss = train()
     print('Epoch: {:02d}, Loss: {:.4f}'.format(epoch, loss))
 acc = test()
@@ -69,3 +70,21 @@ colors = [
     '#ffc0cb', '#bada55', '#008080', '#420420', '#7fe5f0', '#065535', '#ffd700'
 ]
 plot_points(colors)
+
+def plot_points():
+    x = model(torch.arange(data.num_nodes, device=device))
+    x = x.cpu().detach().numpy()
+    y = data.y.cpu().detach().numpy()
+
+    colors = ['#ffc0cb','#bada55','#008080','#420420','#7fe5f0','#065535','#ffd700']
+
+    tsne = TSNE(n_components=2)
+    x_dim_reduc = tsne.fit_transform(x)
+
+    plt.figure(figsize=(8,8))
+    for color, i in zip(colors, [0, 1, 2, 3, 4, 5, 6]):
+        plt.scatter(x_dim_reduc[y == i, 0], x_dim_reduc[y == i, 1], s=20, color=color)
+    plt.axis('off')
+    plt.savefig('node2vec_visualization.png', bbox_inches='tight')
+
+plot_points()
